a8d06c46f3bea754013009fc2249d55ac8fdc77a
Tahmid Mahmud <tmahmud94@gmail.com>
2015-10-29 00:12:47 -0500

    adding stacks

---
+++ Stacks/main/TMStack.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tahmid on 10/28/2015.
+ */
+public class TMStack<T> {
+}
---
+++ Stacks/tests/TMStackTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by tahmid on 10/28/2015.
+ */
+public class TMStackTest {
+
+    private TMStack<Integer> stack = new TMStack<>();
+
+
+}
